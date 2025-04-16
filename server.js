4c53a979fcd2763ce7f3793fc901d171f56f637d
Oğuzhan Tekelioğlu <130230042+OguzhanTekeli06@users.noreply.github.com>
2025-04-16 14:12:38 +0300

    Added and updated endpoints for surveys

--- server.js
+++ server.js
@@ -493,6 +493,90 @@ async function refreshToken(req, res) {
     }
 }
 
+async function surveyGetAllForUser(req, res, next) {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+        const responses = await io.timeout(2000000).emitWithAck("survey:getAllForUser", { token });
+        console.log('Received responses:', JSON.stringify(responses, null, 2));
+
+        // Gelen verilerde internshipPeriod alanı, eğer bir obje olarak geldiyse, yalnızca adını alıyoruz.
+        const formattedSurveys = responses.map(survey => {
+            // Eğer internshipPeriod populate edilmişse ve name içeriyorsa:
+            if (survey.internshipPeriod && survey.internshipPeriod.name) {
+                survey.internshipPeriod = survey.internshipPeriod.name;
+            }
+            return survey;
+        });
+
+        res.send({
+            success: true,
+            data: formattedSurveys
+        });
+    } catch (error) {
+        console.error('Error or timeout:', error);
+        
+        res.send({
+            success: false,
+            message: "Anketler alınırken bir hata oluştu: " + error.message,
+            error: error
+        });
+    }
+    return next();
+}
+
+async function answerAdd(req, res, next) {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+        const { survey, answers } = req.body;
+
+        const response = await io.timeout(2000000).emitWithAck("survey:answerAdd", { token, survey, answers });
+
+        console.log("Received response:", JSON.stringify(response, null, 2));
+
+        res.send(response);
+    } catch (error) {
+        console.error("Error or timeout:", error);
+        res.send({
+            success: false,
+            message: "Anket cevap eklenirken bir hata oluştu: " + error.message,
+            error: error
+        });
+    }
+    return next();
+}
+
+async function surveyGetOne(req, res, next) {
+    try {
+        const { id } = req.params;
+        if (!id) {
+            return res.send({
+                success: false,
+                message: "Geçersiz anket ID'si."
+            });
+        }
+
+        const response = await io
+            .timeout(2000000)
+            .emitWithAck("survey:getOne", { id });
+
+        console.log("Received response:", JSON.stringify(response, null, 2));
+        res.send(response);
+    } catch (error) {
+        console.error("Error or timeout:", error);
+        res.send({
+            success: false,
+            message: "Anket alınırken bir hata oluştu: " + error.message,
+            error: error
+        });
+    }
+    return next();
+}
+
+
+
+
+
+
 //Profile----------------------------------------------------------------------------
 async function getProfile(req, res, next) {
     try {
@@ -508,7 +592,7 @@ async function getProfile(req, res, next) {
       const response = await io.timeout(2000000).emitWithAck("profile:getProfile", { token });
   
       console.log("Received response:", JSON.stringify(response, null, 2));
-      res.send(response);
+      res.send(response[0]);
 
     } catch (error) {
       console.error("Error or timeout:", error);
@@ -535,7 +619,7 @@ async function getProfile(req, res, next) {
     const response = await io.timeout(2000000).emitWithAck("profile:getInternCount", { token });
 
     console.log("Received response:", JSON.stringify(response, null, 2));
-    res.send(response);
+    res.send(response[0]);
   } catch (error) {
     console.error("Error or timeout:", error);
     res.send({
@@ -561,7 +645,7 @@ async function getLeaveCountByMentor(req, res, next) {
       const response = await io.timeout(2000000).emitWithAck("profile:getLeaveCount", { token });
   
       console.log("Received response:", JSON.stringify(response, null, 2));
-      res.send(response);
+      res.send(response[0]);
     } catch (error) {
       console.error("Error or timeout:", error);
       res.send({
@@ -587,7 +671,7 @@ async function getLeaveCountByMentor(req, res, next) {
       const response = await io.timeout(2000000).emitWithAck("profile:getSurveyCount", { token });
   
       console.log("Received response:", JSON.stringify(response, null, 2));
-      res.send(response);
+      res.send(response[0]);
     } catch (error) {
       console.error("Error or timeout:", error);
       res.send({
